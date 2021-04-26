04d9aa2d5461a4b010371a0e54b8505203e24a84
krishpranav <krisna.pranav@gmail.com>
2021-04-26 13:10:48 +0530

    updated relative to absolute

--- builder.py
+++ builder.py
@@ -27,3 +27,21 @@ def absolute_root(url):
         return url
     else:
         return url[:url.find('/',8)]
+
+# relatvie to absolute
+def relative_to_absolute(url, link):
+    if link.startswith('http://') or link.startswith('https://') or link.startswith('data:'):
+        # already absolute, skipping
+        return link
+    if link.startswith('//'):
+        # just add protocol
+        link = 'http:' + link
+        return link
+    if link[0] == '/':
+        # Absolute URL
+        link = absolute_root(url) + link
+        return link
+    else:
+        # Relative URL
+        link = relative_root(url) + link
+        return link
